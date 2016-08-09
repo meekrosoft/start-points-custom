fe0ea70fa06d0da0b18148443134d324347d9636
emilybache <emily@bacheconsulting.com>
2016-08-09 16:06:14 +0200

    added dummy code to prevent compiler error

--- DeviceDriver/C++/MockMemoryDevice.cpp
+++ DeviceDriver/C++/MockMemoryDevice.cpp
@@ -10,9 +10,18 @@ MockMemoryDevice::MockMemoryDevice()
 
 char MockMemoryDevice::read(long address)
 {
-    return 0;
+    // this code is here to prevent compiler errors due to unused variables.
+    // TODO: write a proper implementation 
+    return (char)address;
 }
 
 void MockMemoryDevice::write(long address, char data)
 {
+	// this code is here to prevent compiler errors due to unused variables.
+    // TODO: write a proper implementation 
+    int a = (int)data;
+    int b = (int)address;
+    if (a == b)
+        a = b;
+
 }
