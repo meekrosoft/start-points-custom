92e834c9acdf9c212f9716071fe7007a16f74cfe
emilybache <emily.bache@praqma.com>
2017-06-13 09:29:12 +0200

    added more languages for GildedRose

---
+++ GildedRose/Javascript/gilded_rose_spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let Item, Shop = require('./gilded_rose.js');
+
+
+describe("Gilded Rose", function() {
+
+  it("should foo", function() {
+    const gilgedRose = new Shop([ new Item("foo", 0, 0) ]);
+    const items = gilgedRose.updateQuality();
+    expect(items[0].name).toEqual("fixme");
+  });
+
+});
